68907a7b6d3ef97682f8e023b281c7a922379249
pesabreu <pesabreu@gmail.com>
2018-02-07 20:32:57 -0200

    page header index footer e remoção de arquivos de configuração

--- index.php
+++ index.php
@@ -2,19 +2,23 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \pesabreu\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	//echo "Ecommerce Pesabreu WMS <br /><br />";
-	
-	$sql = new pesabreu\DB\Sql();
-	
-	$res = $sql->select("SELECT * FROM tb_users");
-	
-	echo json_encode($res);
+	//echo "Ecommerce Pesabreu WMS <br /><br />";	
+	/*$sql = new pesabreu\DB\Sql();	
+	$res = $sql->select("SELECT * FROM tb_users");	
+	echo json_encode($res);*/
+
+	$page = new Page();
+
+	$page->setTpl("index");
 
 });
 
